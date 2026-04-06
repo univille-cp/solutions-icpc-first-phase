c38dd539c8c12cdebcd3b9285835b226e469e75d
Leanderson André <leanderson.andre@gmail.com>
2026-04-06 15:24:46 -0300

    Update A.py

--- 2025/A.py
+++ 2025/A.py
@@ -1,1 +1,19 @@
-
+linha = input()
+lista = linha.split(" ")
+N = int(lista[0])
+M = int(lista[1])
+G = [[0 for _ in range(M)] for _ in range(N)]
+for i in range(N):
+    linha = input()
+    lista = linha.split(" ")
+    for j in range(M):
+        G[i][j] = int(lista[j])
+    
+total = 0
+for i in range(M):
+    maior = -1
+    for j in range(N):
+        maior = max(maior,G[j][i])
+    total = total + maior
+    
+print(total)
